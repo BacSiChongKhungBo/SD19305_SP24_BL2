30ea3c5e29b61f73757c28574cb7399ab156f8a3
BacSiChongKhungBo <huywewe123@gmail.com>
2024-03-07 09:40:09 +0700

    Add project files.

---
+++ SD19305_Bai1/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace SD19305_Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txt_MSV = new TextBox();
+            txt_hoten = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            rtxt_dchi = new RichTextBox();
+            label4 = new Label();
+            label5 = new Label();
+            rbtn_nam = new RadioButton();
+            rbtn_Nu = new RadioButton();
+            rbtn_khac = new RadioButton();
+            label6 = new Label();
+            cbx_CSharp = new CheckBox();
+            cbx_SQL = new CheckBox();
+            cbx_Java = new CheckBox();
+            label7 = new Label();
+            btn_Show = new Button();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            rbtn_TMDT = new RadioButton();
+            rbtn_PTPM = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Mã Sinh Viên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Ngày Sinh";
+            // 
+            // txt_MSV
+            // 
+            txt_MSV.Location = new Point(157, 52);
+            txt_MSV.Name = "txt_MSV";
+            txt_MSV.Size = new Size(508, 23);
+            txt_MSV.TabIndex = 3;
+            // 
+            // txt_hoten
+            // 
+            txt_hoten.Location = new Point(157, 98);
+            txt_hoten.Name = "txt_hoten";
+            txt_hoten.Size = new Size(508, 23);
+            txt_hoten.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(157, 141);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // rtxt_dchi
+            // 
+            rtxt_dchi.Location = new Point(157, 186);
+            rtxt_dchi.Name = "rtxt_dchi";
+            rtxt_dchi.Size = new Size(508, 96);
+            rtxt_dchi.TabIndex = 6;
+            rtxt_dchi.Text = "";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(48, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(43, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(48, 295);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Giới tính";
+            // 
+            // rbtn_nam
+            // 
+            rbtn_nam.AutoSize = true;
+            rbtn_nam.Location = new Point(157, 293);
+            rbtn_nam.Name = "rbtn_nam";
+            rbtn_nam.Size = new Size(51, 19);
+            rbtn_nam.TabIndex = 9;
+            rbtn_nam.TabStop = true;
+            rbtn_nam.Text = "Nam";
+            rbtn_nam.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Nu
+            // 
+            rbtn_Nu.AutoSize = true;
+            rbtn_Nu.Location = new Point(274, 293);
+            rbtn_Nu.Name = "rbtn_Nu";
+            rbtn_Nu.Size = new Size(41, 19);
+            rbtn_Nu.TabIndex = 10;
+            rbtn_Nu.TabStop = true;
+            rbtn_Nu.Text = "Nữ";
+            rbtn_Nu.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_khac
+            // 
+            rbtn_khac.AutoSize = true;
+            rbtn_khac.Location = new Point(389, 293);
+            rbtn_khac.Name = "rbtn_khac";
+            rbtn_khac.Size = new Size(51, 19);
+            rbtn_khac.TabIndex = 11;
+            rbtn_khac.TabStop = true;
+            rbtn_khac.Text = "Khác";
+            rbtn_khac.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(48, 332);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Ngành";
+            // 
+            // cbx_CSharp
+            // 
+            cbx_CSharp.AutoSize = true;
+            cbx_CSharp.Location = new Point(154, 376);
+            cbx_CSharp.Name = "cbx_CSharp";
+            cbx_CSharp.Size = new Size(41, 19);
+            cbx_CSharp.TabIndex = 15;
+            cbx_CSharp.Text = "C#";
+            cbx_CSharp.UseVisualStyleBackColor = true;
+            // 
+            // cbx_SQL
+            // 
+            cbx_SQL.AutoSize = true;
+            cbx_SQL.Location = new Point(274, 376);
+            cbx_SQL.Name = "cbx_SQL";
+            cbx_SQL.Size = new Size(47, 19);
+            cbx_SQL.TabIndex = 16;
+            cbx_SQL.Text = "SQL";
+            cbx_SQL.UseVisualStyleBackColor = true;
+            // 
+            // cbx_Java
+            // 
+            cbx_Java.AutoSize = true;
+            cbx_Java.Location = new Point(389, 376);
+            cbx_Java.Name = "cbx_Java";
+            cbx_Java.Size = new Size(48, 19);
+            cbx_Java.TabIndex = 17;
+            cbx_Java.Text = "Java";
+            cbx_Java.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(49, 377);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Môn học";
+            // 
+            // btn_Show
+            // 
+            btn_Show.Location = new Point(527, 315);
+            btn_Show.Name = "btn_Show";
+            btn_Show.Size = new Size(226, 80);
+            btn_Show.TabIndex = 19;
+            btn_Show.Text = "Hiển Thị";
+            btn_Show.UseVisualStyleBackColor = true;
+            btn_Show.Click += btn_Show_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(48, 419);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(705, 150);
+            dataGridView1.TabIndex = 20;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbtn_TMDT);
+            groupBox1.Controls.Add(rbtn_PTPM);
+            groupBox1.Location = new Point(143, 318);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(348, 52);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            // 
+            // rbtn_TMDT
+            // 
+            rbtn_TMDT.AutoSize = true;
+            rbtn_TMDT.Location = new Point(131, 14);
+            rbtn_TMDT.Name = "rbtn_TMDT";
+            rbtn_TMDT.Size = new Size(56, 19);
+            rbtn_TMDT.TabIndex = 22;
+            rbtn_TMDT.TabStop = true;
+            rbtn_TMDT.Text = "TMĐT";
+            rbtn_TMDT.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_PTPM
+            // 
+            rbtn_PTPM.AutoSize = true;
+            rbtn_PTPM.Location = new Point(14, 14);
+            rbtn_PTPM.Name = "rbtn_PTPM";
+            rbtn_PTPM.Size = new Size(56, 19);
+            rbtn_PTPM.TabIndex = 22;
+            rbtn_PTPM.TabStop = true;
+            rbtn_PTPM.Text = "PTPM";
+            rbtn_PTPM.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 581);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(btn_Show);
+            Controls.Add(label7);
+            Controls.Add(cbx_Java);
+            Controls.Add(cbx_SQL);
+            Controls.Add(cbx_CSharp);
+            Controls.Add(label6);
+            Controls.Add(rbtn_khac);
+            Controls.Add(rbtn_Nu);
+            Controls.Add(rbtn_nam);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(rtxt_dchi);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(txt_hoten);
+            Controls.Add(txt_MSV);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_MSV;
+        private TextBox txt_hoten;
+        private DateTimePicker dateTimePicker1;
+        private RichTextBox rtxt_dchi;
+        private Label label4;
+        private Label label5;
+        private RadioButton rbtn_nam;
+        private RadioButton rbtn_Nu;
+        private RadioButton rbtn_khac;
+        private Label label6;
+        private CheckBox cbx_CSharp;
+        private CheckBox cbx_SQL;
+        private CheckBox cbx_Java;
+        private Label label7;
+        private Button btn_Show;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private RadioButton rbtn_TMDT;
+        private RadioButton rbtn_PTPM;
+    }
+}
---
+++ SD19305_Bai1/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace SD19305_Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //label
+            //textbox
+            
+        }
+
+        private void btn_Show_Click(object sender, EventArgs e)
+        {
+            txt_MSV.Text = "PH12345";
+            txt_hoten.Text = "Quỳnh Bei";
+            rtxt_dchi.Text = "FBT Polyme";
+            rbtn_nam.Checked = true;
+            rbtn_TMDT.Checked = true;
+            cbx_CSharp.Checked = true;
+            dateTimePicker1.Value = DateTime.Now;
+            dtgDisplay();
+        }
+
+        public void dtgDisplay()
+        {
+            //B1:xác định số cột
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Mã Sinh Viên";
+            dataGridView1.Columns[1].Name = "Họ Tên";
+            dataGridView1.Columns[2].Name = "Địa Chỉ";
+            dataGridView1.Columns[3].Name = "Giới tính";
+            //B2:Dòng
+            dataGridView1.Rows.Add(txt_MSV.Text, 
+                                   txt_hoten.Text, 
+                                   rtxt_dchi.Text, 
+                                   rbtn_nam.Text);
+        }
+    }
+}
